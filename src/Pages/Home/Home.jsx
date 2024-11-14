d17f38002bc2823d1674dee30166a10de2525502
Enyewashebr <Enyewashebir0@gmail.com>
2024-11-14 09:34:14 +0300

    add more cards

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import './Home.css'
 import Navbar from '../../components/Navbar/Navbar'
-import herro_banner from "../../../public/hero_banner.jpg";
+import herro_banner from "/hero_banner.jpg";
 import hero_title from "../../assets/hero_title.png";
 import play_icon from "../../assets/play_icon.png";
 import info_icon from "../../assets/info_icon.png";
@@ -32,6 +32,12 @@ const Home = () => {
             <TitleCards />
           </div>
         </div>
+      </div>
+      <div className="more-cards">
+        <TitleCards />
+        <TitleCards />
+        <TitleCards />
+        <TitleCards />
       </div>
     </div>
   );
