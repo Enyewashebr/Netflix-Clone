de8ca477219b4e184ba73cc3a63fba93b5c93e70
Enyewashebr <Enyewashebir0@gmail.com>
2024-11-14 23:43:08 +0300

    integrating id of each movie and make the movie thumbnail clickable and display on player pages to play thier trailer

--- src/components/TitleCards/TitleCards.jsx
+++ src/components/TitleCards/TitleCards.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useRef, useState } from "react";
 import "./TitleCards.css";
-import cards_data from "../../assets/cards/Cards_data";
+// import cards_data from "../../assets/cards/Cards_data";
+import { Link } from "react-router-dom";
 
 const TitleCards = ({ title, category }) => {
   const [apiDta, setApiData] = useState([]);
@@ -38,13 +39,13 @@ const TitleCards = ({ title, category }) => {
       <div className="card-list" ref={cardsRef}>
         {apiDta.map((card, index) => {
           return (
-            <div className="card" key={index}>
+            <Link to={`/player/${card.id}`} className="card" key={index}>
               <img
                 src={`https://image.tmdb.org/t/p/w500`+card.backdrop_path}
                 alt=""
               />
               <p>{card.original_title}</p>
-            </div>
+            </Link>
           );
         })}
       </div>
