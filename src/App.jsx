a64280955b9058e881904a0126ef8ec9f61712de
Enyewashebr <Enyewashebir0@gmail.com>
2024-11-14 22:29:50 +0300

    adding different functionality for player page

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@
 import Home from "./Pages/Home/Home"
 import {Routes, Route} from 'react-router-dom'
 import Login from './Pages/Login/Login'
+import Player from "./Pages/Player/Player";
 
 const App = () => {
   return (
@@ -10,6 +11,7 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/player/:id" element={<Player />} />
       </Routes>
     </div>
   );
