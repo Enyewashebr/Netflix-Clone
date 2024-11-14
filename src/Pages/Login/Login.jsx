83a11b8c40564fd6535b06c54465e70b0fc546b6
Enyewashebr <Enyewashebir0@gmail.com>
2024-11-14 16:42:29 +0300

    adding some styles of login page

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,13 +1,19 @@
 //import React from 'react'
 import './Login.css'
 import logo from "../../assets/logo.png";
+import { useState } from 'react';
 
 const Login = () => {
+
+
+const [signState, setSignState] = useState('Sign In')
+
+
   return (
     <div className="login">
       <img src={logo} className="login-logo" alt="" />
       <div className="login-form">
-        <h1>Sign In</h1>
+        <h1>{signState}</h1>
         <form>
           <input type="text" placeholder="Your Name" alt="" />
           <input type="email" placeholder="Your Email" alt="" />
